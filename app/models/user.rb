f607a52d7e36b4b10e8921c731c3ad5f0109628d
s-loar <sjl7909@gmail.com>
2021-08-03 14:48:26 -0400

    Added a check for presence in the model; set an order for the User data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  after_create_commit { broadcast_append_to "users" }
+  validates_presence_of :name
+
+  after_create_commit { broadcast_prepend_to "users" }
   after_update_commit { broadcast_replace_to "users" }
   after_destroy_commit { broadcast_remove_to "users" }
 end
--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -1,7 +1,7 @@
 class UserService 
   def self.find_users(name = nil)
-    return User.all if name.blank?
+    return User.all.order(:created_at) if name.blank?
 
-    User.where("name LIKE :name", name: "%#{ name }%")
+    User.where("name LIKE :name", name: "%#{ name }%").order(:created_at)
   end
 end
